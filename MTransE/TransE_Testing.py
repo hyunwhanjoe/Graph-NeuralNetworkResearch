ed726751076ebabe0dcef6a1fd42869d34a9f3ba
hyunwhanjoe <hyunwhanjoe@gmail.com>
2020-05-25 22:27:06 +0900

    fixed bug where cand was hitting incorrectly and wrapped score into a method

--- MTransE/TransE_Testing.py
+++ MTransE/TransE_Testing.py
@@ -1,12 +1,8 @@
-"""
-TransE testing
-"""
 import sys
 import time
 import numpy as np
 
 sys.path.insert(0, '../../src/TransE')
-print(sys.path)
 
 from TransE import TransE
 
@@ -16,13 +12,14 @@ model = TransE()
 model.load(fmodel)
 
 
-def seem_hit(x, y):
-    for i in y:
-        if x.find(i) > -1 or i.find(x) > -1:
-            return True
-    return False
+def modify_score(score, past_num, tmp_score):
+    if len(score) == 0:
+        score.append(tmp_score)
+    else:
+        score[0] = (score[0] * past_num + tmp_score) / (past_num + 1.0)
 
 
+# fmap = "../../data/WK3l-15k/en_de/P_en_v6_sample_test.csv"
 fmap = "../../data/WK3l-15k/en_de/P_en_v6_test.csv"
 topK = 10
 past_num = 0
@@ -35,40 +32,41 @@ for line in open(fmap):
     h = model.entity_vec(line[0])
     r = model.relation_vec(line[1])
     if h is None or r is None:
-        print("Is not in training set", line[0], line[1])
+        print("Head, relation is not in training set", line[0], line[1])
+        tmp_score = np.zeros(topK)
+        modify_score(score, past_num, tmp_score)
+        past_num += 1
         continue
+
     cand = model.kNN_entity(h + r)  # t - h
     cand = [x[0] for x in cand]
-    print(past_num, line[0], line[1], line[2])
-    print(cand)
 
     tmp_score = np.zeros(topK)
     hit = False
     last_i = 0
     tgt = line[2]
-    if tgt == None:
+    if tgt is None:
+        print("Tail is not in training set", line[2])
+        tmp_score = np.zeros(topK)
+        modify_score(score, past_num, tmp_score)
+        past_num += 1
         continue
 
     for i in range(len(cand)):
-        last_i = i
         tmp_cand = cand[i]
-        if hit == False and (seem_hit(tmp_cand, tgt) == True):
+        if (hit is False) and (tmp_cand == tgt):
             hit = True
-        if hit == True:
+        if hit:
             tmp_score[i] = 1.0
 
-    while last_i < topK:  # dont know what the purpose for this is
-        if hit:
-            tmp_score[last_i] = 1.0
-        last_i += 1
+    modify_score(score, past_num, tmp_score)
 
-    if len(score) == 0:
-        score.append(tmp_score)
-    else:
-        score[0] = (score[0] * past_num + tmp_score) / (past_num + 1.0)
-    print(score)
+    if past_num % 100 == 0:
+        print(past_num, line[0], line[1], line[2])
+        print(cand)
+        print(score)
+        print(time.time() - start_time)
     past_num += 1
 
-# sys.path.remove('')
-# print sys.path
+print(score)
 print(time.time() - start_time)
