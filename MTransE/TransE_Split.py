71e25261133eba575f473ff61559919118d9f0d5
hyunwhanjoe <hyunwhanjoe@gmail.com>
2020-06-05 23:03:55 +0900

    Added utf8 encoding, wrapped writing code into a with statement

--- MTransE/TransE_Split.py
+++ MTransE/TransE_Split.py
@@ -1,30 +1,26 @@
 """
 Split into training and test set
-Double check for off by one errors
 """
 import random
-path = "../../data/WK3l-15k/en_de/P_en_v6.csv"
+path = "data/WK3l-15k/en_de/P_en_v6.csv"
 triples = []
-with open(path, "r") as file:
+with open(path, "r", encoding="utf8") as file:
     for line in file:
         line = line.rstrip()
         triples.append(line)
     random.shuffle(triples)
 
-test_path = "../../data/WK3l-15k/en_de/P_en_v6_test.csv"
-test = open(test_path, "w")
-training_path = "../../data/WK3l-15k/en_de/P_en_v6_training.csv"
-training = open(training_path, "w")
+test_path = "data/WK3l-15k/en_de/P_en_v6_test.csv"
+training_path = "data/WK3l-15k/en_de/P_en_v6_training.csv"
+with open(test_path, "w", encoding="utf8") as test, \
+        open(training_path, "w", encoding="utf8") as training:
+    test_percent = 0.1
+    i = 0
+    while i < (len(triples) * 0.1):
+        test.write(triples[i]+"\n")
+        i += 1
 
-test_percent = 0.1
-i = 0
-while i < len(triples) * 0.1:
-    test.write(triples[i]+"\n")
-    i += 1
-test.close()
-
-print(i)
-while i < len(triples):
-    training.write(triples[i]+"\n")
-    i += 1
-training.close()
+    print(i)
+    while i < len(triples):
+        training.write(triples[i]+"\n")
+        i += 1
