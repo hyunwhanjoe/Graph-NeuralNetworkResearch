5c849637f96d165af76aa9aff1d9a4f097b38fbf
hyunwhanjoe <hyunwhanjoe@gmail.com>
2020-06-16 22:41:42 +0900

    l2_normalize each row in weight matrix so norm for each row is 1

--- MTransE/TransE.py
+++ MTransE/TransE.py
@@ -1,7 +1,7 @@
 import time
 import tensorflow as tf
 
-from MTransE.OneHotEncoding import one_hot_encode
+from MTransE.Encoding import encode
 from tensorflow.keras import Model
 from tensorflow.keras import layers
 
@@ -10,6 +10,8 @@ from tensorflow.keras import layers
 class TransELayer(layers.Layer):
     def __init__(self, units):
         super(TransELayer, self).__init__()
+        self.w = None
+        self.b = None
         self.units = units
 
     def build(self, input_shape):
@@ -17,13 +19,7 @@ class TransELayer(layers.Layer):
         self.w = tf.Variable(initial_value=w_init(shape=(input_shape[-1], self.units),
                                                   dtype='float32'),
                              trainable=True)
-        #  Normalize
-        for i in range(input_shape[-1]):
-            self.w[i].assign(self.w[i] / tf.norm(self.w[i]))
-
-        self.b = self.add_weight(shape=(self.units,),
-                                 initializer='zeros',
-                                 trainable=False)
+        self.w = tf.math.l2_normalize(self.w, axis=1)
 
     def call(self, inputs):
         return tf.matmul(inputs, self.w)  # + self.b
@@ -48,7 +44,7 @@ def main():
     t0 = time.time()
     print(tf.__version__)
     path = "data/WK3l-15k/en_de/test.csv"
-    ds_train = tf.data.Dataset.from_tensor_slices(one_hot_encode(path))  # mini-batch?
+    ds_train = tf.data.Dataset.from_tensor_slices(encode(path, "one-hot"))  # mini-batch?
     transe_model = TransE()
     optimizer = tf.keras.optimizers.SGD()  # ()
 
@@ -63,20 +59,18 @@ def main():
             optimizer.apply_gradients(zip(grads, transe_model.trainable_variables))
         loss_state.update_state(loss)
 
-    for epoch in range(1, 20):
-        epoch_loss_total = tf.keras.metrics.Sum()
-        for datum in iter(ds_train):
-            train(datum, epoch_loss_total)
-        print("Epoch %d loss %.3f" % (epoch, epoch_loss_total.result()))
-    print("Time used ", time.time() - t0)
-    # for datum in iter(ds_train):
-    #     h, r, t = transe_model(datum)
-    #     print(h)
-    #     print(tf.norm(h))
-    #     h /= tf.norm(h)
-    #     print(h)
-    #     print(tf.norm(h))
-    #     break
+    # for epoch in range(1, 20):
+    #     epoch_loss_total = tf.keras.metrics.Sum()
+    #     for datum in iter(ds_train):
+    #         train(datum, epoch_loss_total)
+    #     print("Epoch %d loss %.3f" % (epoch, epoch_loss_total.result()))
+    # print("Time used ", time.time() - t0)
+    for datum in iter(ds_train):
+        h, r, t = transe_model(datum)
+        print(tf.norm(h))
+        print(tf.norm(r))
+        print(tf.norm(t))
+        break
 
 
 if __name__ == "__main__":
