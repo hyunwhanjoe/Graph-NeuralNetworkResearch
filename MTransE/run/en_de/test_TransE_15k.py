2b93416f319eed7b0be2441ddef34bfccf7b26f2
hyunwhanjoe <hyunwhanjoe@gmail.com>
2020-06-23 23:03:04 +0900

    Vectorize distance comparison of h + r and other entities - bug with removing h

--- MTransE/run/en_de/test_TransE_15k.py
+++ MTransE/run/en_de/test_TransE_15k.py
@@ -1,7 +1,44 @@
 import time
 import tensorflow as tf
+import numpy as np
 from MTransE.TransEHelper import TransEHelper
 from MTransE.TransE_Embedding import TransEmbedding
+
+
+def test(model, helper, ds_test):
+    entity_count = helper.get_entity_count()
+    indexes = tf.convert_to_tensor(np.arange(entity_count))
+    all_entity_embeddings, p, o = model((indexes, indexes, indexes))
+    topK = 10
+    past_num = 0
+    score = []
+
+    for inputs in iter(ds_test):
+        inputs = (inputs[:, 0], inputs[:, 1], inputs[:, 2])
+
+        h_index = int(inputs[0].numpy()[0])
+        t0 = all_entity_embeddings[:h_index]
+        t1 = all_entity_embeddings[h_index + 1:]
+        print("all", all_entity_embeddings)
+        print("t0", t0)
+        print("t1", t1)
+        entity_embeddings = tf.concat([t0, t1], 0)
+        print(entity_embeddings)
+        h, r, t = model(inputs)
+        h_plus_r = h + r
+
+        distance = tf.reduce_sum(tf.pow((h_plus_r - entity_embeddings), 2.0), -1)
+        cand = tf.argsort(distance, axis=-1, direction='ASCENDING')[:10]
+        print(helper.decode_entity(inputs[0].numpy()[0]))
+        print(helper.decode_relation(inputs[1].numpy()[0]))
+        print(helper.decode_entity(inputs[2].numpy()[0]), "\n")
+        # bug
+        for index in cand:
+            entity_index = index.numpy()
+            if entity_index >= h_index:
+                print(helper.decode_entity(entity_index+1))
+            else:
+                print(helper.decode_entity(entity_index))
 
 
 def main():
@@ -11,24 +48,17 @@ def main():
     helper = TransEHelper()
     helper.generate_vocab(training_path)
     numpy = helper.encode_vocab(test_path)
-    ds_test = tf.data.Dataset.from_tensor_slices(numpy).batch(32768)
+    ds_test = tf.data.Dataset.from_tensor_slices(numpy).batch(1)
     ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
 
     model = TransEmbedding(helper.get_entity_count(), 75)
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
     checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
     manager = tf.train.CheckpointManager(
-        checkpoint, directory="../../models/en_de", max_to_keep=5)
+        checkpoint, directory="../../models/en_de/400", max_to_keep=5)
     checkpoint.restore(manager.latest_checkpoint)
 
-    for inputs in iter(ds_test):
-        inputs = (inputs[:, 0], inputs[:, 1], inputs[:, 2])
-        print(inputs)
-        # print(helper.decode_relation(inputs[1][0]))
-        # print(helper.decode_entity(inputs[2][0]))
-        h, r, t = model(inputs)
-        distance = tf.norm(h + r - t, axis=1)
-        print(distance)
+    test(model, helper, ds_test)
 
 
 if __name__ == "__main__":
